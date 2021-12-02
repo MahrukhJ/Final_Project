4726a464769a868dc071dc4268901c3fbb9b3f0e
Mahrukh A. Jaura <mahrukh.jaura@Mahrukhs-Air.home>
2021-12-02 13:38:22 -0500

    -.-

--- Final Project Script.R
+++ Final Project Script.R
@@ -1,2 +1,41 @@
 Mahrukh Jaura
 Final Project Draft (for now)
+
+I think it would be interesting to investigate the relationship between income and life expectancy and 
+examining the factors that can lead to an increase or decrease in life expectancy.
+'Introduction:'
+Life expectancy has increased significantly over the past centuries. This paper will study the impact of 
+income inequality on life expectancy along with observing other variables that may play a significant factor. 
+
+One of the first studies that established a relationship between income inequality and life expectancy 
+measured the level, temporal trend, and geographical variability in the association between income and 
+life expectancy while identifying factors related to small area variation within the association. 
+After evaluating factors associated with differences in life expectancy and estimating said expectancy by 
+household income percentile, sex & geographic area, the study found that higher income was associated with 
+higher life expectancy throughout the income distribution.
+  
+'Literature Review:'
+A notable study that established the relationship between income and life expectancy written in 2016 by Chetty et al., 
+The Association Between Income and Life Expectancy in the United States, 2001 – 2014, measured the level, temporal trend, 
+and geographical variability in the association between income and life expectancy while identifying factors related to 
+small area variation within the association. Income data for the U.S. population was obtained from 1.4 billion de-identified
+tax records from 1999-2014; mortality data obtained through the Social Security Administration death records. With the objective 
+of evaluating factors associated with differences in life expectancy and estimating said expectancy by household income percentile, 
+sex & geographic area, the study found that higher income was associated with higher life expectancy throughout the income distribution. 
+Inequality in life expectancy increased over time, life expectancy for low-income individuals was positively correlated with the local 
+area fraction of immigrants, college graduates & local government expenditures and geographic differences in life expectancy for 
+individuals in the lowest income quartile were significantly correlated with smoking but not significantly correlated with access to 
+medical care, labor market conditions or income inequality. 
+
+'Data:'
+attach(dat3)
+table(dat3$INCOME2, dat3$EDUCA)
+Analyzing the table that depicts income across education levels leads to the observation that as a persons level of education attained increases, 
+the larger the number of people who earn $75,000 or more, will be, for that category. Reasonably, higher education levels can lead to better 
+occupations and a healthier lifestyle, which can contribute to longer life expectancy.
+
+
+
+
+
+
